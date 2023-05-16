ea478536aca6bf0d0c7c3ab84fa1a927a5811fae
sousegi <sousegi@gmail.com>
2023-05-16 08:25:02 +0300

    fix article controller #1

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::get('/articles', [ArticleController::class, 'show']);
+Route::post('/auth/logout', [AuthController::class, 'logoutUser']);
+Route::get('/article/{id}', [ArticleController::class, 'article']);
+Route::post('/article/create', [ArticleController::class, 'store']);
+Route::get('/myarticles', [ArticleController::class, 'myArticles']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/auth/logout', [AuthController::class, 'logoutUser']);
-    Route::get('/article/{id}', [ArticleController::class, 'article']);
-    Route::post('/article/create', [ArticleController::class, 'store']);
-    Route::get('/myarticles', [ArticleController::class, 'myArticles']);
+
 });
