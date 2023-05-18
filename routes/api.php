8df31023436bfef992abc9e6837ce8b153349132
Eugen Sosnovschi <eugen.sosnovschi@ourbox.org>
2023-05-18 15:59:12 +0300

    fix articles collection #1

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
+Route::get('/articles', [ArticleController::class, 'getAllArticles']);
 
 
 
